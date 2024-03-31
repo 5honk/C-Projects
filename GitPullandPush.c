d17a7260f153d3a426f205e8f70b75fbfcde944a
pfarquharson <Penelope.Farquharson@protonmail.com>
2024-03-31 03:19:28 +0100

    Changes to file contents

--- GitPullandPush.c
+++ GitPullandPush.c
@@ -15,10 +15,13 @@ This file is a demonstration to using GIT for the first time :D!
 3. In order to push to a Github repository we need the repositorys https link. This is found in the green "<> Code" button, under HTTPS.
     git remote add origin 'add the https link here'
 
-4. We can finally push the file 
+4. !!! Make sure to commit the files or else it wount upload 
+    git commit -m 'commit message goes here e.g. Updating software pacthes'
+
+5. We can finally push the file 
     git push -u origin main
 
-5. A prompt will appear to enter your Github username and password. Since Github does'nt use the passwords for authentication, a token will need to be generated as the password
+6. A prompt will appear to enter your Github username and password. Since Github does'nt use the passwords for authentication, a token will need to be generated as the password
     /Settings/DeveloperSettings/Personal Access Token/ either fine-graned token or Token classic/ select all git options/ copy and paste the token to terminal
 */
 
